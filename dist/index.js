9bae78b5d89a8a76b027346976cef445c9f8ca00
Henrik Knudsen <henrik.knudsen@bouvet.no>
2022-01-03 12:49:44 +0100

    Output to dist. Update tsc target.

---
+++ dist/index.js
@@ -0,0 +1,35 @@
+import { createConfigItem, loadPartialConfig, transformAsync, } from "@babel/core";
+import { transform } from "@svgr/core";
+import { promises as fs } from "fs";
+import presetReact from "@babel/preset-react";
+import presetEnv from "@babel/preset-env";
+import pluginTransformReactConstantElements from "@babel/plugin-transform-react-constant-elements";
+module.exports = function (_snowpackConfig, _pluginOptions) {
+    return {
+        name: "snowpack-svgr-plugin",
+        resolve: {
+            input: [".svg"],
+            output: [".js"],
+        },
+        async load({ filePath }) {
+            const contents = await fs.readFile(filePath, "utf-8");
+            const code = await transform(contents, { icon: true, exportType: "named" }, { componentName: "ReactComponent" });
+            const babelOptions = {
+                babelrc: false,
+                configFile: false,
+                presets: [
+                    createConfigItem(presetReact, { type: "preset" }),
+                    createConfigItem([presetEnv, { modules: false }], { type: "preset" }),
+                ],
+                plugins: [createConfigItem(pluginTransformReactConstantElements)],
+            };
+            const babelConfig = loadPartialConfig({
+                filename: filePath,
+                ...babelOptions,
+            });
+            const transformOptions = babelConfig?.options;
+            const result = (await transformAsync(code, transformOptions)) || {};
+            return result.code;
+        },
+    };
+};
